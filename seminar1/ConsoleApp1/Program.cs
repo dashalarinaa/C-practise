6ad8ef36a555e34bbb5ecf4f1ee52f205aa87595
Dasha Larina <larinadg@gmail.com>
2023-06-10 23:55:07 +0300

    add task 1

---
+++ seminar1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+
+Console.Write("Введи целое число");
+
+int number = int.Parse(Console.ReadLine());
+
+// Квадрат входного числа
+int result = number * number;
+
+//Вывод результата
+Console.WriteLine($"Квадрат входного числа: {result}");
---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход 
+//принимает два числа и проверяет, является ли первое 
+//число квадратом второго
+
+Console.Write("Введи первое целое число");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введи второе целое число");
+int number_2 = int.Parse(Console.ReadLine());
+
+int number_3 = number_2 * number_2;
+
+if (number_3 == number) 
+{
+    Console.Write("Первое число является квадратом второго");
+}
+else
+{
+    Console.Write("Первое число не является квадратом второго");
+}

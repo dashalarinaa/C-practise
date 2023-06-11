5a3d39c2932f0b17ab6d655cdfc70206850dd98c
Dasha Larina <larinadg@gmail.com>
2023-06-11 17:30:45 +0300

    add task2

--- seminar1/task2/Program.cs
+++ seminar1/task2/Program.cs
@@ -3,3 +3,33 @@
 // 3 -> Среда 
 // 5 -> Пятница
 
+Console.Write("Введи целое число от 1 до 7 включительно");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case (1):
+        Console.WriteLine("Понедельник");
+        break;
+    case (2):
+        Console.WriteLine("Вторник");
+        break;
+    case (3):
+        Console.WriteLine("Среда");
+        break;
+    case (4):
+        Console.WriteLine("Четверг");
+        break;
+    case (5):
+        Console.WriteLine("Пятница");
+        break;
+    case (6):
+        Console.WriteLine("Суббота");
+        break; 
+    case (7):
+        Console.WriteLine("Воскресенье");
+        break;               
+    default:
+        Console.WriteLine("Такого дня недели не существует");
+        break;
+}

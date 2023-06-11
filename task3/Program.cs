8eada859af10b617e19df8d585754453e30855a8
Dasha Larina <larinadg@gmail.com>
2023-06-11 18:26:00 +0300

    add task4

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введи целое трехзначное число");
+double number = double.Parse(Console.ReadLine());
+
+if (99 < number && number < 1000)
+{
+double number_1 = number % 10;
+Console.WriteLine($"Последняя цифра числа равна: {number_1}");
+}
+
+else
+{
+    Console.WriteLine("Не попали в диапазон");
+}

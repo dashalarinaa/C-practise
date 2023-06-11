1316fd640198d23bda4105beb38cceb9cce1b239
Dasha Larina <larinadg@gmail.com>
2023-06-11 19:51:15 +0300

    add homework task4

---
+++ homework_seminar1/hm_task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введи целое число");
+int number = int.Parse(Console.ReadLine());
+
+int number_2 = 1; 
+
+while (number_2 < number)
+{
+    number_2 % 2 == 0;
+    number_2 = number_2 + 1;
+}

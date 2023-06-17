13d88ab89667da07ac637356f04e5cef7d36c900
Dasha Larina <larinadg@gmail.com>
2023-06-17 20:57:21 +0300

    add task 3

---
+++ homework_seminar3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+double GetCube(int i)
+{
+    double result = i*i*i;
+    return result;
+}
+Console.Write("Введи число N");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    double finish = GetCube(i);
+    Console.WriteLine(finish);
+}

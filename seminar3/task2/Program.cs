54dfe5cdc1a52c84ebe697607fb90d3427e61e73
Dasha Larina <larinadg@gmail.com>
2023-06-17 19:24:02 +0300

    add correct task 2

--- seminar3/task2/Program.cs
+++ seminar3/task2/Program.cs
@@ -14,10 +14,6 @@ int x2 = int.Parse(Console.ReadLine());
 Console.Write("Введи координату y второго числа");
 int y2 = int.Parse(Console.ReadLine());
 
-
-double a = Math.Pow((x1 - x2), 2);
-double b = Math.Pow((y1 - y2), 2);
-
-double result = Math.Sqrt(( a + b));
+double result = Math.Sqrt(Math.Pow((x1-x2), 2) + Math.Pow((y1-y2), 2));
 
 Console.WriteLine($"Расстояние между точками равно {result}");

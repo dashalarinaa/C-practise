32a34b636561c432aa21b9fc631afc2d424bd710
Dasha Larina <larinadg@gmail.com>
2023-06-17 19:37:02 +0300

    add task 3

---
+++ seminar3/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введи число");
+int N = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while ( count <= N)
+{
+    double result = Math.Pow(count, 2);
+    count++;
+    Console.WriteLine($"{result}");
+}

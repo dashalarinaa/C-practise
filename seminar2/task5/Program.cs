1bca9ccd7f8460601fd0ce468758dc0a5a4a6c44
Dasha Larina <larinadg@gmail.com>
2023-06-17 13:28:24 +0300

    add task 5

---
+++ seminar2/task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа на вход принимает число (может быть любой размерности). Необходимо вывести количество цифр в этом числе.
+
+Console.Write("Введи целое число");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+if (number > 0)
+{
+    while (number / 10 > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+}
+else
+{
+    while (number / -10 > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+}    
+Console.Write($"Количество цифр в числе - {count}");

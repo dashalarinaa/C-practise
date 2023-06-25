41b91cee9a470f52df947fe75ab92c13caa9e584
Dasha Larina <larinadg@gmail.com>
2023-06-25 15:10:23 +0300

    add task1

---
+++ seminar4/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+int i = 0;
+while (number > 0)
+{
+    number = number / 10;
+    i++;
+}
+Console.WriteLine(i); 

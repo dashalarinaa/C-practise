f4670d3ac3f801c65d4fe6706c65af861b0dae30
Dasha Larina <larinadg@gmail.com>
2023-06-25 16:15:03 +0300

    add task 2

---
+++ seminar4/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120 */
+
+int GetFactorial(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+         result = result * i;
+    }
+    return result;
+}
+
+Console.Write("Введи число: ");
+int input_number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetFactorial(input_number));

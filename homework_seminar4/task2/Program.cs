db0adf79a03b40f59406c68155755737a3003c22
Dasha Larina <larinadg@gmail.com>
2023-06-25 17:11:27 +0300

    add task 2

---
+++ homework_seminar4/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSum(int number)
+{
+int i = 1;
+int result = 0;
+while (number > 0)
+{
+    result = result + (number % 10);
+    number = number / 10;
+    i++;
+}
+return result;
+}
+Console.Write("Введи число: ");
+int input_number = int.Parse(Console.ReadLine());
+Console.WriteLine(GetSum(input_number));

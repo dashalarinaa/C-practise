07671d01886d7bf466f03ad5cfaf586387da399e
Dasha Larina <larinadg@gmail.com>
2023-06-28 00:16:20 +0300

    add task 3

---
+++ homework_seminar5/task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = new int [5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next (1, 100);
+    Console.WriteLine (array[i]);
+}
+
+int max = array[1];
+int min = array[1];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i]; 
+    }
+
+     if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($" Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+ // int diff = max - min; Console.WriteLine($" Разница между максимальным и минимальным элементом массива равно {diff}");

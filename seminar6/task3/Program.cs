5a137133cc8f71d61c2efef21a7027482230f4f4
Dasha Larina <larinadg@gmail.com>
2023-07-02 22:32:28 +0300

    add task 3

---
+++ seminar6/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+Console.Write("Введи целое число ");
+int number = int.Parse(Console.ReadLine());
+string sum ="";
+int step = 0;
+while (number > 0)
+{
+    step = number % 2;
+    number = number / 2;
+    sum = step + sum;
+}
+Console.Write($"В двоичной системе число = {sum}");

0ad838219f8eb33ecb7b3d34dc578a396f2d60e0
Dasha Larina <larinadg@gmail.com>
2023-07-04 22:13:41 +0300

    add task 2

---
+++ seminar7/task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+System.Console.Write("Enter num rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter num columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void PrintArray(int [,] matr)
+{
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int [,] matr)
+{
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matr[i,j] = i + j;
+    }
+}
+}
+int[,] matrix = new int[m, n];
+PrintArray(matrix);
+Console.WriteLine("");
+FillArray(matrix);
+PrintArray(matrix);
+

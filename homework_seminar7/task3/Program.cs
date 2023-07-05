decdaec92f22133b4e228c2b8e8c8082d212738b
Dasha Larina <larinadg@gmail.com>
2023-07-05 22:53:51 +0300

    add task 3

---
+++ homework_seminar7/task3/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+System.Console.Write("Enter num rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter num columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void PrintArray(int [,] matr)
+{
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int [,] matr)
+{
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matr[i,j] = new Random().Next(1, 10);
+    }
+}
+}
+
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine("");
+
+
+for (int j = 0; j < n; j++)
+{
+    double average = 0;
+    for (int i = 0; i < m; i++)
+    {
+        average = average + matrix[i,j];
+    }
+    average = Math.Round(average / m, 2);
+    Console.Write(average + " ");
+}    
+    

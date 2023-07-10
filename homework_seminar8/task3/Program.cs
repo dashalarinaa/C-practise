a4996913254311655bc6add3a159db2ccb7e9e32
Dasha Larina <larinadg@gmail.com>
2023-07-10 23:59:55 +0300

    add task 3

--- homework_seminar8/task3/Program.cs
+++ homework_seminar8/task3/Program.cs
@@ -4,15 +4,13 @@
 
 System.Console.Write("Enter num rows1: ");
 int m = Convert.ToInt32(Console.ReadLine());
-
 System.Console.Write("Enter num columns1: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
 System.Console.Write("Enter num rows2: ");
 int m2 = Convert.ToInt32(Console.ReadLine());
-
 System.Console.Write("Enter num columns2: ");
 int n2 = Convert.ToInt32(Console.ReadLine());
+
 void PrintArray(int [,] matr)
 {
 for (int i = 0; i < m; i++)
@@ -35,6 +33,7 @@ for (int i = 0; i < m; i++)
     }
 }
 }
+
 void PrintArray2(int [,] matr)
 {
 for (int i = 0; i < m2; i++)
@@ -66,3 +65,32 @@ Console.WriteLine("");
 FillArray2(matrix2);
 PrintArray2(matrix2);
 Console.WriteLine("");
+
+ void Multiplication(int[,] matrixA, int[,] matrixB)
+    
+    {  
+        if (matrixA.GetLength(0) != matrixB.GetLength(1))
+    {
+        Console.WriteLine("Перемножение невозможно");
+    }     
+        int matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+        for (int i = 0; i < GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixB.GetLength(1); j++)
+            {
+                matrixC[i, j] = 0;
+
+                for (int k = 0; k < matrixA.GetLength(0); k++)
+                {
+                    matrixC[i, j] = matrixC[i, j] + matrixA[i, k] * matrixB[k, j];
+                }
+            }
+        }
+
+        return matrixC;
+    }
+var A = matrix1[m,n];
+var B = matrix2[m2,n2];
+var result = Multiplication(A, B);
+Console.WriteLine(result);

7609d00b4d2cbd11a8ebdb29d9455ea0c3d39fb1
Dasha Larina <larinadg@gmail.com>
2023-07-16 21:21:30 +0300

    add task 3

---
+++ seminar9/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+Console.Write("Введи целое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введи целое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+int FillNatural(int M, int N)
+{
+    if (M -1  == N) return M;
+    int temp = 1 + FillNatural(M, N-1);
+    Console.WriteLine(N + "");
+    return temp;
+}
+
+FillNatural(number, number_2);

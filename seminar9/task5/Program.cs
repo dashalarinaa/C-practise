effcfbee50bd10f589d16b630efeaf55f1ed6cdb
Dasha Larina <larinadg@gmail.com>
2023-07-16 21:55:21 +0300

    add task 5

---
+++ seminar9/task5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число A в целую степень B с помощью рекурсии
+
+int GetRecStepen(int num_1, int num_2)
+{
+if (num_2 == 0) return 1;
+int temp = GetRecStepen(num_1, num_2) * num_1;
+return temp;  
+}
+
+Console.Write("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetRecStepen(number_1, number_2));
+
+// доделать
+

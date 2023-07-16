a0fbfcbfb034ec8a488fb5444fb9ad24f980a6c5
Dasha Larina <larinadg@gmail.com>
2023-07-16 22:01:15 +0300

    add task 2

---
+++ homework_seminar9/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetRecSum(int number)
+{
+if (number == 0) return 0;
+int temp = number % 10 + GetRecSum(number / 10);
+return temp;
+}
+Console.Write("Введи число: ");
+int input_number = int.Parse(Console.ReadLine());
+Console.WriteLine(GetRecSum(input_number));

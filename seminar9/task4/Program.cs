9ab09fe26597d66a921b9074d7955e59be70e952
Dasha Larina <larinadg@gmail.com>
2023-07-16 21:38:29 +0300

    add task 4

---
+++ seminar9/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int GetRecSum(int number)
+{
+if (number == 0) return 0;
+int temp = number % 10 + GetRecSum(number / 10);
+return temp;
+}
+Console.Write("Введи число: ");
+int input_number = int.Parse(Console.ReadLine());
+Console.WriteLine(GetRecSum(input_number));

2dac95d6dfb700ac7cc365233197fd2c9dceca36
Dasha Larina <larinadg@gmail.com>
2023-07-16 21:58:52 +0300

    add task 1

---
+++ homework_seminar9/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введи целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int FillNatural(int N)
+{
+    if (N == 0) return 1;
+    Console.WriteLine(N + "");
+    int temp = 1 + FillNatural(N-1);
+
+    return temp;
+}
+
+FillNatural(number);
